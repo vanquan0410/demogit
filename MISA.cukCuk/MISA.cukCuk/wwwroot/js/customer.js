e372efceee6bded0b56a3bf179c5d43c79c41f21
dam van quan <vanquan4101999@gmail.com>
2020-09-20 22:43:29 +0700

    update new

---
+++ MISA.cukCuk/MISA.cukCuk/wwwroot/js/customer.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+/*  loadData();*/
+    alert("hello");
+    $('#btnAdd').click(function () {
+        $('.form-dialog').show();
+    })
+
+    $('#btnCancel').click(function () {
+        $('.form-dialog').hide();
+    })
+  /*  var employee = new employeejs();*/
+   
+
+})
+class customerjs {
+    constructor() {
+        this.initevent();
+    }
+
+    initevent() {
+        debugger
+        $("#btnAdd").click(this.btnAddOnClick.bind(this));
+    }
+    btnAddOnClick() {
+        debugger
+        this.showDialogDetail();
+    }
+
+    showDialogDetail() {
+        $('.form-dialog').show();
+    }
+}
